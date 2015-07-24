6a48d978964b5936d1f3fcf69b4e6511306cb097
cdriscol <drizkol@gmail.com>
2015-07-24 08:29:40 -0600

    moving student row into directive

--- app/students/directives/student.row.client.directive.js
+++ app/students/directives/student.row.client.directive.js
@@ -3,17 +3,18 @@
 
     angular
         .module('mg')
-        .directive('createStudent', createStudent);
+        .directive('studentRow', studentRow);
 
-    function createStudent() {
+    function studentRow() {
         var directive = {
             restrict: 'EA',
-            templateUrl: 'app/students/views/student.create.view.html',
-            link: linkFunc,
+            templateUrl: 'app/students/views/student.row.view.html',
             scope: {
               student: '=',
-              addStudent: '&'
+              saveStudent: '&',
+              deleteStudent: '&'
             },
+            link: linkFunc,
             controller: Controller,
             controllerAs: 'vm',
             bindToController: true
@@ -21,7 +22,9 @@
 
         return directive;
 
-        function linkFunc(scope, el, attr) {}
+        function linkFunc(scope, el, attr, ctrl) {
+
+        }
     }
 
     function Controller() {
